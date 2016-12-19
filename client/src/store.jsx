c341027211dbdb1134f66db4673d91fa54b34679
XianAquino <xian_a@yahoo.com>
2016-12-19 17:00:18 -0600

    passed eslint test

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -1,20 +1,21 @@
 import { createStore, combineReducers } from 'redux'
 
 const pulseData = (state = [0], action) => {
-  let last = state[state.length-1];
+  let last = state[ state.length - 1 ]
   switch (action.type) {
     case 'INCREMENT':
       return state.concat([++last])
     case 'DECREMENT':
-      if(last !== 0){
-      return state.concat([--last])
+      if (last !== 0) {
+        return state.concat([--last])
       }
+      break
     default:
       return state
   }
 }
 
-//store all reducers in one variable
+// store all reducers in one variable
 const combinedReducers = combineReducers({
   pulseData
 })
