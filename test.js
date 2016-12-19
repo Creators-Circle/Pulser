1cf4ec78208a839043fe01b969636cb167db4352
Hack Reactor Students <accounts@hackreactor.com>
2016-12-19 16:33:18 -0600

    stubbed tests

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
 //tests go here
+
+//sample test to check functionality of suite
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+describe('increment', function() {
+    // pending test below
+    it('should return -1 when the value is not present');
+  });
+
+describe('decrement', function() {
+    // pending test below
+    it('should return -1 when the value is not present');
+  });
