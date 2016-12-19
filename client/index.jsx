1b02901e804a9db40ae387621b21036e90b4ecaa
XianAquino <xian_a@yahoo.com>
2016-12-19 11:37:33 -0600

    imported Redux & Router

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
 
-window.onload = () => {
-  ReactDOM.render()
-}
+import store from './store.jsx';
+import App from './components/App.jsx';
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store = {store}>
+      <Router>
+        <Route path="/" component={App} />
+        {/* {fill in with other components that needs a specif route} */}
+      </Router>
+    </Provider>
+  ,document.getElementById('app'));
+};
+
+store.subscribe(render);
+render();
