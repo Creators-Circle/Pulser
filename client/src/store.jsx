b78b6cfecf9129231823206b8b346b1ab2b51453
Ari Frankel <ari.l.frankel@gmail.com>
2016-12-19 15:48:25 -0600

    added reducer for handling pulse data

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -1,8 +1,21 @@
 import { createStore, combineReducers } from 'redux';
 
+//reducer for incrememnt/decrement
+const pulseData = (state, action) => {
+  let last = state[state.length-1]
+  switch (action.type) {
+    case 'increment':
+      return state.concat([last++])
+    case 'decrement':
+      return state.concat([last--])
+    default:
+      return state;
+  }
+}
+
 //store all reducers in one variable
 const combinedReducers = combineReducers({
-  //fill in with reducers
+  pulseData
 });
 
 const store = createStore(combinedReducers);
