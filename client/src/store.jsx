d56e46be490b9a1ba894b429bae2aa64e1bba201
Hack Reactor Students <accounts@hackreactor.com>
2016-12-19 17:48:27 -0600

    fixed bugs in reducer and added test.js

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -1,15 +1,16 @@
 import { createStore, combineReducers } from 'redux'
 
+//reducer for INCREMENT/DECREMENT
 const pulseData = (state = [0], action) => {
   let last = state[ state.length - 1 ]
   switch (action.type) {
     case 'INCREMENT':
       return state.concat([++last])
     case 'DECREMENT':
-      if (last !== 0) {
-        return state.concat([--last])
-      }
-      break
+      if(last !== 0){
+      return state.concat([--last])
+      } 
+      return state.concat([0]);
     default:
       return state
   }
--- test.js
+++ test.js
@@ -1,28 +1,52 @@
-//tests go here
+deepFreeze = require('deep-freeze')
 
-//sample test to check functionality of suite
+const pulseData = (state = [0], action) => {
+  let last = state[state.length-1];
+  switch (action.type) {
+    case 'INCREMENT':
+      return state.concat([++last])
+    case 'DECREMENT':
+      if(last !== 0){
+      return state.concat([--last])
+      } 
+      return state.concat([0]);
+    default:
+      return state;
+  }
+}
+  //pulseData
+
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Reducers', function() {
+  describe('pulseData', function() {
+    beforeEach(function(){
+      let testState = [1,2,3,4,5], testStateLength = testState.length
+      deepFreeze(testState); // don't allow testState to be overwritten
+    })
+    // it('should preserve the original state', function() { //run the function, then check whether the imput is mutated
+    //   //should be handled by deepFreeze
+    // });
+    describe('INCREMENT', function() {
+      it('last value should be one greater than the immediately preceding', function() {
+        assert.equal(6, pulseData(testState, 'INCREMENT')[testStateLength-1])
+      });
+
+      it('new array should be longer by 1', function() {
+        assert.equal(pulseData(testState, 'INCREMENT').length, testStateLength+1);
+      });
+
     });
+
+    describe('DECREMENT', function() {
+      it('last value should be one less than the immediately preceding', function() {
+        assert.equal(4, pulseData(testState, 'INCREMENT')[testStateLength-1])      
+      });
+
+      it('new array should be longer by 1', function() {
+        let lengthBefore = testState.length;
+        assert.equal(pulseData(testState, 'INCREMENT').length, lengthBefore+1);
+      });
+    });
+
   });
 });
-
-describe('increment', function() {
-    // pending test below
-    // feed it an array
-    // we want to get back an array that is one longer
-    // last value should be one more 
-    it('should return -1 when the value is not present');
-  });
-
-describe('decrement', function() {
-    // pending test below
-    // pending test below
-    // feed it an array
-    // we want to get back an array that is one longer
-    // last value should be one last 
-    it('should return -1 when the value is not present');
-  });
