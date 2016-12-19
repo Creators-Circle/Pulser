6c771b9054a464190a608646423f9194f80f6da4
XianAquino <xian_a@yahoo.com>
2016-12-19 11:20:03 -0600

    Created redux storage

---
+++ client/store.jsx
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+
+//store all reducers in one variable
+const combinedReducers = combineReducers({
+  //fill in with reducers
+});
+
+
+const store = createStore(combinedReducers);
+
+export default store;
