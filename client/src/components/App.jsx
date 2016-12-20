9a7d0610a9a67015b8a17717be6fc1ff17ac1df1
Johnny McDuff <jtmcduff@gmail.com>
2016-12-20 15:08:21 -0600

    Updated stuff.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,7 +15,7 @@ import { Link } from 'react-router';
 // import FeedbackBox from './FeedbackBox'
 // import PulseBox from './PulseBox'
 
-//  Links route users to pulsebox and feedbackbox as appropriate.
+//  Links route users to PresenterView and AudienceView as appropriate.
 class App extends Component {
   render () {
     console.log(this.props);
