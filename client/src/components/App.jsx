f94def12d9bb7101c8f6fb86d890ed33b0de91b2
Johnny McDuff <jtmcduff@gmail.com>
2016-12-20 08:32:16 -0600

    Done with App links/Skeleton.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,21 +9,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Slides from './Slides';
-//  Allows html-like link functionality.
-import { Link } from 'react-router';
+//Allows html-like link functionality.
+import { Link } from 'react-router'
 
 // import FeedbackBox from './FeedbackBox'
 // import PulseBox from './PulseBox'
 
-//  Links route users to PresenterView and AudienceView as appropriate.
+// Links route users to pulsebox and feedbackbox as appropriate.
 class App extends Component {
   render () {
     console.log(this.props);
     return (
       <div>
         <p>Hello World!</p>
-        <li><Link to="/presenter">Presenter</Link></li>
-        <li><Link to="/audience">Audience</Link></li>
+        <li><Link to="/pulsebox">Presenter</Link></li>
+        <li><Link to="/feedbackbox">Audience</Link></li>
       </div>
     );
   };
