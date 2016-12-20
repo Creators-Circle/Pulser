7b2c6f7ceaaa86694e8c01c9ed105a08e495bb8a
Johnny McDuff <jtmcduff@gmail.com>
2016-12-20 11:27:21 -0600

    Fixed eslint and npm test.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,8 +4,8 @@ import Slides from './Slides';
 //Allows html-like link functionality.
 import { Link } from 'react-router'
 
-import FeedbackBox from './FeedbackBox'
-import PulseBox from './PulseBox'
+// import FeedbackBox from './FeedbackBox'
+// import PulseBox from './PulseBox'
 
 //Links route users to pulsebox and feedbackbox as appropriate.
 class App extends Component {
