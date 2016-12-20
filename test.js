d3f6bc6aded2090ca0fcd10353f4c46d22630048
Hack Reactor Students <accounts@hackreactor.com>
2016-12-19 18:51:11 -0600

    tests working

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-deepFreeze = require('deep-freeze')
+const deepFreeze = require('deep-freeze')
 
 const pulseData = (state = [0], action) => {
   let last = state[state.length-1];
@@ -19,32 +19,41 @@ const pulseData = (state = [0], action) => {
 var assert = require('assert');
 describe('Reducers', function() {
   describe('pulseData', function() {
-    beforeEach(function(){
-      let testState = [1,2,3,4,5], testStateLength = testState.length
-      deepFreeze(testState); // don't allow testState to be overwritten
-    })
     // it('should preserve the original state', function() { //run the function, then check whether the imput is mutated
     //   //should be handled by deepFreeze
     // });
     describe('INCREMENT', function() {
+      beforeEach(function(){
+      var testState = [1,2,3,4,5], testStateLength = testState.length
+      deepFreeze(testState); // don't allow testState to be overwritten
+    });
+      
       it('last value should be one greater than the immediately preceding', function() {
-        assert.equal(6, pulseData(testState, 'INCREMENT')[testStateLength-1])
+        var testState = [1,2,3,4,5], testStateLength = testState.length
+        deepFreeze(testState); // don't allow testState to be overwritten
+        assert.equal(6, pulseData(testState, {type: 'INCREMENT'})[testStateLength])
       });
 
       it('new array should be longer by 1', function() {
-        assert.equal(pulseData(testState, 'INCREMENT').length, testStateLength+1);
+        var testState = [1,2,3,4,5], testStateLength = testState.length
+        deepFreeze(testState); // don't allow testState to be overwritten
+        assert.equal(pulseData(testState, {type: 'INCREMENT'}).length, testStateLength+1);
       });
 
     });
 
     describe('DECREMENT', function() {
       it('last value should be one less than the immediately preceding', function() {
-        assert.equal(4, pulseData(testState, 'INCREMENT')[testStateLength-1])      
+        var testState = [1,2,3,4,5], testStateLength = testState.length
+        deepFreeze(testState); // don't allow testState to be overwritten
+        assert.equal(4, pulseData(testState, {type: 'DECREMENT'})[testStateLength])      
       });
 
       it('new array should be longer by 1', function() {
+        var testState = [1,2,3,4,5], testStateLength = testState.length
         let lengthBefore = testState.length;
-        assert.equal(pulseData(testState, 'INCREMENT').length, lengthBefore+1);
+        deepFreeze(testState); // don't allow testState to be overwritten
+        assert.equal(pulseData(testState, {type: 'DECREMENT'}).length, lengthBefore+1);
       });
     });
 
