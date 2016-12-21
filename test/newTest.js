60cddb1299d3476c2edff56de78976b019cf2178
Your Name <sheel.bedi@gmail.com>
2016-12-21 14:50:46 -0600

    tests folder is now separate

--- test/newTest.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return the index when the value is present', function() {
-      assert.equal(0, [1,2,3].indexOf(1));
-    });
-  });
-});
