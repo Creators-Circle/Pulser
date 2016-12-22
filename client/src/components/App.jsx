e8034f419a8a7e0a1eaad511a2b35d16bdadf77f
Johnny McDuff <jtmcduff@gmail.com>
2016-12-22 13:32:59 -0600

    Updated App with comment.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,9 +14,6 @@ import { Link } from 'react-router';
 
 import getUserData from '../util/getUserData';
 
-// import FeedbackBox from './FeedbackBox'
-// import PulseBox from './PulseBox'
-
 // Links route users to pulsebox and feedbackbox as appropriate.
 class App extends Component {
 
