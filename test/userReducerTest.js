ee9fa2e93ed9d8013e1b8a1746088c4d9f7b3098
XianAquino <xian_a@yahoo.com>
2016-12-21 20:02:03 -0600

    test for changing user role

--- test/userReducerTest.js
+++ test/userReducerTest.js
@@ -2,19 +2,29 @@ const assert = require('assert');
 import  user  from '../client/src/reducers/userReducer.jsx';
 
 describe('userReducer', function() {
-  var testState = {};
-
-  describe('STORE USER', function(){
-    it('it should store user\'s information', function(){
-      assert.deepEqual({name:"Alice", email: "alice@y.com",role:'presenter', avatar:"alice_photo"},
-        user(testState, {type: 'STORE_USER',name:"Alice", email: "alice@y.com", role:'presenter', avatar:"alice_photo"}))
-    });
-  });
+  var testState = {name:"Alice", email: "alice@y.com",role:'presenter', avatar:"alice_photo"}
 
   describe('UNDEFINED ACTION', function(){
-    it('should return default state if action is undefined', function(){
+    it('should return default state if action is undefined', function () {
       assert.deepEqual(testState, user(testState, {type: 'TEST'}));
     });
   });
+
+  describe('STORE_USER', function () {
+    it('it should store user\'s information', function () {
+      assert.deepEqual(testState,
+        user(testState, {type: 'STORE_USER',name:"Alice", email: "alice@y.com", role:'presenter', avatar:"alice_photo"}));
+    });
+  });
+
+  describe('CHANGE_ROLE', function () {
+    it('it should change the role of the user', function(){
+      assert.deepEqual({name:"Alice", email: "alice@y.com",role:'audience', avatar:"alice_photo"},
+        user(testState, {type: 'CHANGE_ROLE', role: 'audience'}));
+    });
+  });
+
+
+
 
 });
