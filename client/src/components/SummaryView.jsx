5603fa58016b983969276f95cc5ab03e35eb7bd3
Ari Frankel <ari.l.frankel@gmail.com>
2016-12-29 13:51:05 -0600

    alert clicks on name click

--- client/src/components/SummaryView.jsx
+++ client/src/components/SummaryView.jsx
@@ -12,16 +12,23 @@ class SummaryView extends Component {
     // Render a table of users and their total clicks
     for (var user in this.props.usersClicks) {
       $('#usersClicks').find('tr:gt(0)').remove();
-      $('#usersClicks tr:last').after(`<tr><td>${user}</td><td>${this.props.usersClicks[user].length}</td></tr>`);
+      $('#usersClicks tr:last').after(`<tr><td><a class="user" data-user=${user.split(' ').join('_')}>${user}</a></td><td>${this.props.usersClicks[user].length}</td></tr>`);
     }
+    // Pull out the usersClicks from the this context of React for use in jQuery
+    var usersClicks = this.props.usersClicks;
+    $('.user').on('click', function () {
+        alert(usersClicks[$(this).data('user').split('_').join(' ')]);
+      });
     return (
       <div>
         <Link to="/">Home</Link>
         <table id="usersClicks">
+        <tbody>
           <tr>
             <th>User</th>
             <th>Clicks</th>
           </tr>
+        </tbody>
         </table>
       </div>
     );
