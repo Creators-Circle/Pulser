5065bc7100e5bc28d484ea33e1ef5fe960ad0fd9
Ari Frankel <ari.l.frankel@gmail.com>
2016-12-29 13:00:22 -0600

    SummaryView with total clicks per user

--- client/src/components/SummaryView.jsx
+++ client/src/components/SummaryView.jsx
@@ -8,23 +8,23 @@ import $ from 'jquery';
 class SummaryView extends Component {
 
   render () {
-    console.log("props in SummaryView: ", this.props)
-    // Render a table of users and their total clicks
-  	for (var user in this.props.usersClicks){
-      $('#usersClicks').find('tr:gt(0)').remove();
-      $('#usersClicks tr:last').after(`<tr><td>${user}</td><td>${this.props.usersClicks[user].length}</td></tr>`);
-    }
-    return (
-  	  <div>
-        <Link to="/">Home</Link>
-  	  	<table id="usersClicks">
-  	  	  <tr>
-  	  	    <th>User</th>
-  	  	    <th>Clicks</th>
-  	  	  </tr>
-  	  	</table>
-  	  </div>
-    )
+  console.log('props in SummaryView: ', this.props);
+  // Render a table of users and their total clicks
+  for (var user in this.props.usersClicks) {
+    $('#usersClicks').find('tr:gt(0)').remove();
+    $('#usersClicks tr:last').after(`<tr><td>${user}</td><td>${this.props.usersClicks[user].length}</td></tr>`);
+  }
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <table id="usersClicks">
+        <tr>
+          <th>User</th>
+          <th>Clicks</th>
+        </tr>
+      </table>
+    </div>
+  );
   }
 }
 
