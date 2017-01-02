69bd126e162dc8f02b3e3dae658076ab365d8d5a
Ari Frankel <ari.l.frankel@gmail.com>
2017-01-02 14:40:28 -0600

    create and join rooms. TODO: refactor emits to broadcasts to specific rooms

--- client/src/components/NewPresButton.jsx
+++ client/src/components/NewPresButton.jsx
@@ -4,9 +4,20 @@ import picker from '../util/googlePicker'; // import Google Picker function
 // button to select Google Slides presentation from user's Google Drive to present
 class NewPresButton extends Component {
 
+  setPresentation () {
+  // Generate a random, 6 character string to name the socket 'room' for that presentation
+  let roomId = (Math.random().toString(36)+'00000000000000000').slice(2, 8);
+  // Join the presenter to that room
+  socket.emit('joinRoom', roomId);
+  //TODO: PUT THE CODE SOMEWHERE
+  }
+
   render () {
     return (
+    <div>
+      <button onClick={this.setPresentation}>TempGenerator</button>
       <button onClick={picker}>New Presentation</button>
+    </div>
     );
   };
 };
