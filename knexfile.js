40fcc70d6c2e5fea0e61c0e54aba71e6b31da23f
XianAquino <xian_a@yahoo.com>
2017-01-03 12:34:01 -0600

    Fixed eslint error

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection:process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     searchPath: 'followme'
   },
 
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection:process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     searchPath: 'followme',
     pool: {
       min: 2,
