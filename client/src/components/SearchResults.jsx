b7d41e66e359898aebba209400fd406d406aad9d
Johnny McDuff <jtmcduff@gmail.com>
2017-01-03 08:31:14 -0600

    Update SearchResults.jsx

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PresThumb from './PresThumb';
 
 // panel to display search results from querying DB for specific presentation data
+  //Rendered by loginDashboard.
 class SearchResults extends Component {
 
   render () {
