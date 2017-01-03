3a392cebb2c80fdcf4e1f4a8a7ab257f5737d544
XianAquino <xian_a@yahoo.com>
2017-01-02 19:27:15 -0600

    saving and fetching user data

--- server/controllers.js
+++ server/controllers.js
@@ -1,1 +1,24 @@
+// js file for accessing the database
 var db = require('./db.js');
+
+module.exports = {
+  getUser: function (req, res) {
+    db('users').where('id', req.session.token)
+      .then(function (data) {
+        var user = data[0];
+        res.send({name: user.name, avatar: user.avatar, email: user.email});
+      });
+  },
+  saveUser: function (user) {
+    db('users').where('id', user.id)
+      .then(function (res) {
+        if (res.length > 0) {
+          // if user exist in the database update user info
+          db('users').where('id', user.id).update(user);
+        } else {
+          // save user info to the database
+          db('users').insert(user);
+        }
+      });
+  }
+};
