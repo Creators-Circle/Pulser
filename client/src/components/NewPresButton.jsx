3701779ca3966377342979a1ca5c1691381c385f
Ari Frankel <ari.l.frankel@gmail.com>
2017-01-03 15:01:51 -0600

    pushing up sockets. no longer works after front end overhaul. TODO: refactor sockets

--- client/src/components/NewPresButton.jsx
+++ client/src/components/NewPresButton.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import picker from '../util/googlePicker'; // import Google Picker function
+import io from 'socket.io-client';
 
 // button to select Google Slides presentation from user's Google Drive to present
 class NewPresButton extends Component {
 
   setPresentation () {
-  // Generate a random, 6 character string to name the socket 'room' for that presentation
-  let roomId = (Math.random().toString(36)+'00000000000000000').slice(2, 8);
-  // Join the presenter to that room
-  socket.emit('joinRoom', roomId);
-  //TODO: PUT THE CODE SOMEWHERE
+    // Generate a random, 6 character string to name the socket 'room' for that presentation
+    let roomId = (Math.random().toString(36) + '00000000000000000').slice(2, 8);
+    // Join the presenter to that room
+    socket.emit('joinRoom', roomId);
+    // TODO: PUT THE CODE SOMEWHERE
   }
 
   render () {
