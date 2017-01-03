01c9a33ff3ccdef9fbdb21df7f6a613e14cea519
XianAquino <xian_a@yahoo.com>
2017-01-03 14:57:31 -0600

    created route for fetching user's presentations

--- server/routes.js
+++ server/routes.js
@@ -3,5 +3,6 @@ var router = require('express').Router();
 var controllers = require('./controllers.js');
 
 router.get('/user', controllers.getUser);
+router.get('/userPresentation', controllers.getUserPresentations);
 
 module.exports = router;
