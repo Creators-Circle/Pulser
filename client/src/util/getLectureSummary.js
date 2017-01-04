a2732e0fb6332939ad2386d143ad065ce68927cb
XianAquino <xian_a@yahoo.com>
2017-01-04 17:55:00 -0600

    helper function for fetching lecture's summary

---
+++ client/src/util/getLectureSummary.js
@@ -0,0 +1,14 @@
+// helper function for fetching summary data
+import $ from 'jquery';
+
+const getLectureSummary = (lectureId, callback) => {
+  $.get(`/api/summary/${lectureId}`)
+    .done((data) => {
+      callback(data);
+    })
+    .fail(({responseJSON})=>{
+    responseJSON.error.errors.forEach((err) =>
+      console.error(err)
+    );
+  });
+}
