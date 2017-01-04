10b71c2fb295c1f609a6ead061c4acf0a3eac734
Ari Frankel <ari.l.frankel@gmail.com>
2017-01-04 11:53:44 -0600

    merge lecturesPresentations into Master

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -6,7 +6,6 @@ import usersClicks from './reducers/usersClicks';
 import pulseData from './reducers/pulseData';
 import presentationsLectures from './reducers/presentationsLectures';
 import userLectures from './reducers/userLectures';
-import presentationsLectures from './reducers/presentationsLectures';
 // store all reducers in one variable
 const combinedReducers = combineReducers({
   pulseData,
