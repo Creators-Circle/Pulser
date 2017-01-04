f977a5777c0091ec7d790a81c8b0038a9c3d9180
Your Name <sheel.bedi@gmail.com>
2017-01-04 16:30:07 -0600

    begin refactor for additional table and columns

--- seeds/seed_name.js
+++ seeds/seed_name.js
@@ -5,29 +5,37 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       return knex('user_lectures').del()
       .then(function () {
-        return knex('users').del()
+        return knex('questions').del()
         .then(function () {
-          return Promise.all([
-          // Inserts seed entries
-            knex('users').insert({id: '114267757081727627546', name: 'Sneaky Ross Topol', avatar: 'https://lh4.googleusercontent.com/-ENvzYvvyW_k/AAAAAAAAAAI/AAAAAAAAEOA/_nB-8dk8arg/photo.jpg', email: 'test@test.mail.com'}),
-            knex('users').insert({id: '110709938641205847283', name: 'Ari Gold Frankel', avatar: 'https://lh5.googleusercontent.com/-F0mWWBnynZI/AAAAAAAAAAI/AAAAAAAAP6o/IXUujroyAlo/photo.jpg', email: 'test@test.mail.com'}),
-            knex('users').insert({id: 'a083b72cbc4b', name: 'Sheel Freeway Rick Bedi', avatar: 'https://avatars.githubusercontent.com/u/13170829?v=3', email: 'sheel.bedi@gmail.com'}),
-            knex('users').insert({id: 'ea2f59c19983', name: 'Christian The Hammer Aquino', avatar: 'https://avatars.githubusercontent.com/u/17533705?v=3', email: 'christianaquino223@gmail.com'}),
-            knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'ea23aa', role: 'presenter'}),
-            knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'ea2355', role: 'presenter'}),
-            knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'cc0001', role: 'presenter'}),
-            knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'ea2351', role: 'audience'}),
-            knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: '12dwwa', role: 'audience'}),
-            knex('user_lectures').insert({user_id: '114267757081727627546', lecture_id: 'cc0001', role: 'audience'}),
-            knex('user_lectures').insert({user_id: '110709938641205847283', lecture_id: 'cc0001', role: 'audience'}),
-            knex('user_lectures').insert({user_id: 'a083b72cbc4b', lecture_id: 'cc0001', role: 'audience'}),
-            knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '114267757081727627546'}),
-            knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '110709938641205847283'}),
-            knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '110709938641205847283'}),
-            knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '114267757081727627546'}),
-            knex('users_clicks').insert({lecture_id: 'cc0001', user_id: 'a083b72cbc4b'}),
-            knex('users_clicks').insert({lecture_id: 'cc0001', user_id: 'a083b72cbc4b'})
-          ]);
+          return knex('users').del()
+          .then(function () {
+            return knex('lectures').del()
+            .then(function () {
+              return Promise.all([
+                knex('lectures').insert({id: 'cc0001', name: 'sample presentation', presentation_id: 'sample1', preview_image: 'https://www.engageselling.com/blog/wp-content/uploads/2014/12/RefreshBLogDecember2nd.jpg'}),
+                knex('lectures').insert({id: 'cc0002', name: 'sample presentation', presentation_id: 'sample2', preview_image: 'https://blognumbers.files.wordpress.com/2010/09/2.jpg'}),
+                knex('users').insert({id: '114267757081727627546', name: 'Sneaky Ross Topol', avatar: 'https://lh4.googleusercontent.com/-ENvzYvvyW_k/AAAAAAAAAAI/AAAAAAAAEOA/_nB-8dk8arg/photo.jpg', email: 'test@test.mail.com'}),
+                knex('users').insert({id: '110709938641205847283', name: 'Ari Gold Frankel', avatar: 'https://lh5.googleusercontent.com/-F0mWWBnynZI/AAAAAAAAAAI/AAAAAAAAP6o/IXUujroyAlo/photo.jpg', email: 'test@test.mail.com'}),
+                knex('users').insert({id: 'a083b72cbc4b', name: 'Sheel Freeway Rick Bedi', avatar: 'https://avatars.githubusercontent.com/u/13170829?v=3', email: 'sheel.bedi@gmail.com'}),
+                knex('users').insert({id: 'ea2f59c19983', name: 'Christian The Hammer Aquino', avatar: 'https://avatars.githubusercontent.com/u/17533705?v=3', email: 'christianaquino223@gmail.com'}),
+                knex('questions').insert({lecture_id: 'ea2f59c19983', user_id: 'ea23aa', question: 'presenter', votes: ''}),
+                knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'ea23aa', role: 'presenter', comment:, no_of_clicks: }),
+                knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'ea2355', role: 'presenter', comment:, no_of_clicks: }),
+                knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'cc0001', role: 'presenter', comment:, no_of_clicks: }),
+                knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: 'ea2351', role: 'audience', comment:, no_of_clicks: }),
+                knex('user_lectures').insert({user_id: 'ea2f59c19983', lecture_id: '12dwwa', role: 'audience', comment:, no_of_clicks: }),
+                knex('user_lectures').insert({user_id: '114267757081727627546', lecture_id: 'cc0001', role: 'audience'}),
+                knex('user_lectures').insert({user_id: '110709938641205847283', lecture_id: 'cc0001', role: 'audience'}),
+                knex('user_lectures').insert({user_id: 'a083b72cbc4b', lecture_id: 'cc0001', role: 'audience'}),
+                knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '114267757081727627546'}),
+                knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '110709938641205847283'}),
+                knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '110709938641205847283'}),
+                knex('users_clicks').insert({lecture_id: 'cc0001', user_id: '114267757081727627546'}),
+                knex('users_clicks').insert({lecture_id: 'cc0001', user_id: 'a083b72cbc4b'}),
+                knex('users_clicks').insert({lecture_id: 'cc0001', user_id: 'a083b72cbc4b'})
+              ]);
+            });
+          });
         });
       });
     });
@@ -66,4 +74,9 @@ exports.seed = function (knex, Promise) {
 //  12dwwa | test server time        | 2017-01-03 22:53:48.70374+00  | 
 //  112355 | test1 order presenttion | 2017-01-03 22:55:19.220139+00 | 
 //  cc0001 | sample presentation     | 2017-01-03 22:58:04.039861+00 | 
+
+// questions
+//  id | lecture_id |   user_id    |    question     | votes 
+// ----+------------+--------------+-----------------+-------
+//   1 | ea23aa     | a083b72cbc4b | is this working |     0
 
