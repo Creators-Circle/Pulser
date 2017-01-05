47c38beb275b4ef7c8069279e7b71f45d34ff238
XianAquino <xian_a@yahoo.com>
2017-01-05 17:29:21 -0600

    route for posting a comment

--- server/routes.js
+++ server/routes.js
@@ -5,5 +5,6 @@ var controllers = require('./controllers.js');
 router.get('/user', controllers.getUser);
 router.get('/userLectures', controllers.getUserLectures);
 router.get('/summary/:lecture_id', controllers.getSummary);
+router.post('/:lecture_id/comment/:user_id', controllers.addComment);
 
 module.exports = router;
