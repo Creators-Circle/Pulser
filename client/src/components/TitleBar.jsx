8496016b8307fb30c823eff25a66e08eabd52aab
Johnny McDuff <jtmcduff@gmail.com>
2017-01-05 15:45:16 -0600

    Done with TitleBar for now.

--- client/src/components/TitleBar.jsx
+++ client/src/components/TitleBar.jsx
@@ -3,20 +3,19 @@
     //  lectureTitle is editable.
   // Displays lectureID.
   // TODO Make Title editable.
-  
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class TitleBar extends Component {
   constructor () {
     super();
-    //Date set-up here so it only calculates once.
+    // Date set-up here so it only calculates once.
     this.d = new Date();
-    this.now = this.d.getHours()+':'+this.d.getMinutes()+'  '+this.d.getDate()+'-'+( this.d.getMonth() + 1)+'-'+this.d.getFullYear();
+    this.now = this.d.getHours() + ':' + this.d.getMinutes() + '  ' + this.d.getDate() + '-' + (this.d.getMonth() + 1) + '-' + this.d.getFullYear();
   }
 
   render () {
-
     return (
       <div>
        <h1> Lecture Title {this.props.activeLecture.name} </h1>
