f9e1af398fc3593c0a832803aceab409a8524b3c
XianAquino <xian_a@yahoo.com>
2017-01-04 23:53:35 -0600

    transfer routes to a separate file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,19 +4,7 @@ import { Router, Route, browserHistory } from 'react-router';
 
 // Redux store name:  store
 import store from './store.jsx';
-import App from './components/App.jsx';
-import PresenterView from './components/PresenterView';
-import AudienceView from './components/AudienceView';
-import SummaryView from './components/SummaryView';
-
-const routes = (
-  <Route>
-    <Route path="/" component={App} />
-    <Route path="/presenter" component={PresenterView} />
-    <Route path="/audience" components={AudienceView} />
-    <Route path="/summary" components={SummaryView} />
-  </Route>
-);
+import routes from './routes.jsx';
 
 const render = () => {
   ReactDOM.render(
