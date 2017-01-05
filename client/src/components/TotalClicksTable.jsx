c9fe3821e4ecc140c6d04922d99430d3a61cdd37
XianAquino <xian_a@yahoo.com>
2017-01-05 12:13:30 -0600

    added event handler for userclicks

--- client/src/components/TotalClicksTable.jsx
+++ client/src/components/TotalClicksTable.jsx
@@ -5,10 +5,14 @@ import { Link } from 'react-router';
 // table to display total number of clicks per user during the presentation
 class TotalClicksTable extends Component {
 
+  displayUserSummary (id) {
+    // test for clicking the user's name or picture, replace with userSummary component
+    console.log('display user');
+  }
+
   render () {
     // pull out the users from the store and filter by audience
     let usersClicks = this.props.summary.users.filter(user => user.role === 'audience');
-
     return (
       <div>
         <Link to="/">Home</Link>
@@ -21,7 +25,9 @@ class TotalClicksTable extends Component {
           {
             usersClicks.map(user =>
             <tr>
-              <td><img id='profilePic' src={user.avatar} /><span>{user.name}</span></td>
+              <td onClick = { () => { this.displayUserSummary(user.user_id); } }>
+                <img id='profilePic' src={user.avatar} /><span>{user.name}</span>
+              </td>
               <td>{user.no_of_clicks}</td>
             </tr>
             )
