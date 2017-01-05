a96d07961336cea014e37d632106ac6af35c9cc7
XianAquino <xian_a@yahoo.com>
2017-01-04 19:02:45 -0600

    rendering summary in the client side

---
+++ client/src/reducers/summary.jsx
@@ -0,0 +1,12 @@
+// reducer for storing lecture's summary
+
+const summary = (state={}, action) => {
+  switch (action.type) {
+    case 'UPDATE_SUMMARY':
+
+    default:
+      return state
+  }
+}
+
+export default summary;
---
+++ test/summary.js
@@ -0,0 +1,18 @@
+// test for summary reducer
+const assert = require('assert');
+import summary from '../client/src/reducers/summary.jsx';
+
+describe('summary', function () {
+  var testState = {user:[{name:'tester'}],clicks:[{'id':'121'}],questions:[{question:"is this working"}]};
+  describe('UNDEFINED ACTION', function(){
+    it('should return default state if action is undefined', function () {
+      assert.deepEqual(testState, search(testState, {type: 'TEST'}));
+    });
+  });
+  describe('UPDATE_SUMMARY', function(){
+    it('should update the value of the state ', function () {
+      assert.deepEqual(testState, search({}, {type: 'UPDATE_SUMMARY', summary:{user:[{name:'tester'}],clicks:[{'id':'121'}],questions:[{question:"is this working"}]}}));
+    });
+  });
+
+});
