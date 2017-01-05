10be518d030d0ec74873088dc5db6e1f30ae31a9
Johnny McDuff <jtmcduff@gmail.com>
2017-01-05 15:42:20 -0600

    TitleBar debugged and linted.

--- client/src/components/TitleBar.jsx
+++ client/src/components/TitleBar.jsx
@@ -3,22 +3,25 @@
     //  lectureTitle is editable.
   // Displays lectureID.
   // TODO Make Title editable.
-
+  
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class TitleBar extends Component {
   constructor () {
     super();
-    this.date = new Date();
+    //Date set-up here so it only calculates once.
+    this.d = new Date();
+    this.now = this.d.getHours()+':'+this.d.getMinutes()+'  '+this.d.getDate()+'-'+( this.d.getMonth() + 1)+'-'+this.d.getFullYear();
   }
 
   render () {
+
     return (
       <div>
-       <h1> Lecture Title {this.props.activeLecture.name}</h1>
+       <h1> Lecture Title {this.props.activeLecture.name} </h1>
        <h2> Join Code {this.props.activeLecture.lectureId} </h2>
-       <h3> Date {this.date} </h3>
+       <h3> Date {this.now} </h3>
       </div>
     );
   };
