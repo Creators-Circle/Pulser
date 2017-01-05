67d120302eb0f16b9f143ca0aee932760e167987
Ross Topol <rtopol2@users.noreply.github.com>
2017-01-04 21:59:28 -0600

    Update NewPresButton.jsx

--- client/src/components/NewPresButton.jsx
+++ client/src/components/NewPresButton.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import picker from '../util/googlePicker'; // import Google Picker function
 // button to select Google Slides presentation from user's Google Drive to present
-NewPresButton = () => {
+const NewPresButton = () => {
   return (
     <button onClick={picker}>New Presentation</button>
   );
