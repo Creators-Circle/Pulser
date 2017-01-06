256d33aa53ccc5afab0a021c3756965f5e0bd1d2
XianAquino <xian_a@yahoo.com>
2017-01-06 13:19:35 -0600

    Update SummaryUser component

---
+++ client/src/components/SummaryUser.jsx
@@ -0,0 +1,12 @@
+// component for display user's summary
+import React, { Component } from 'react';
+
+class SummaryUser extends Component {
+  render () {
+    return (
+      <p>Summary User</p>
+    );
+  }
+}
+
+export default SummaryUser;
