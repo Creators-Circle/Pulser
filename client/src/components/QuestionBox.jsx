8a6bbfd608deb1820ba938572796e02a8db72354
Your Name <sheel.bedi@gmail.com>
2017-01-06 10:09:10 -0600

    stylistic fixes

--- client/src/components/QuestionBox.jsx
+++ client/src/components/QuestionBox.jsx
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 import Question from './Question';
 
 class QuestionBox extends Component {
-//this component lets users enter questions; it also displays each individual question component
+//t his component lets users enter questions; it also displays each individual question component
   constructor () {
     super();
     this.state = {
       questions: [{questionId: 0, questionText: 'first question'}, {questionId: 1, questionText: 'second question'}, {questionId: 3, questionText: 'third question'}]
-    //this is hardcoded - we will need to refactor to use store later
+    // this is hardcoded - we will need to refactor to use store later
     };
   }
 
