f3ea79125509f98d59a6888a3a4e2031f6ca3db6
Ari Frankel <ari.l.frankel@gmail.com>
2017-01-07 11:52:39 -0600

    use store instead of state to render questions - force update

--- client/src/components/QuestionBox.jsx
+++ client/src/components/QuestionBox.jsx
@@ -5,14 +5,6 @@ import $ from 'jquery';
 
 class QuestionBox extends Component {
 // This component lets users enter questions; it also displays each individual question component
-  // constructor () {
-  //   super();
-  //   // REMOVE THE STATE WHEN THE QUESTION REDUCER IS READY AND REPLACE WITH INFORMATION FROM THE STORE
-  //   this.state = {
-  //     // questions: [{questionId: 0, questionText: 'first question'}, {questionId: 1, questionText: 'second question'}, {questionId: 3, questionText: 'third question'}]
-  //     questions: []
-  //   };
-  // };
 
   submitQuestion () {
     console.log('submitQuestion event fired');
@@ -33,8 +25,7 @@ class QuestionBox extends Component {
     };
     socket.emit('submitQuestion', question);
     console.log('submitQuestion sent', question);
-    // let setState = this.setState.bind(this);
-    // let state = this.state;
+    let render = this.forceUpdate.bind(this);
     socket.on('submitQuestion', function (question) {
       console.log('question received', question);
       dispatch({
@@ -42,9 +33,8 @@ class QuestionBox extends Component {
         questionText: question.questionText,
         questionId: question.questionId
       });
-      // setState({questions: state.questions.concat([{questionId: questionId, questionText: questionText}])});
+      render();
     });
-    this.render();
     console.log('this.props after dispatch in submitQuestion', this.props);
   }
 
