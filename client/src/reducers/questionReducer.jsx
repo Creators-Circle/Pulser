d2b84b475c6e2d8aea2a5d463e4fe2e4783b747d
Ross Topol <rtopol2@gmail.com>
2017-01-06 19:19:17 -0600

    Add question reducer and tests for it

---
+++ client/src/reducers/questionReducer.jsx
@@ -0,0 +1,32 @@
+// reducer for creating questions, incrementing votes, and clearing question data
+
+// state = { 
+//   questionId: {votes: #, questionText: 'sample question'},
+//   questionId: {votes: #, questionText: 'sample question'},
+//   questionId: {votes: #, questionText: 'sample question'}
+// }
+
+// action = {type: 'CREATE_QUESTION', questionId: string, questionText: string}
+// action = {type: 'UPVOTE', questionId: string}
+// action = {type: 'CLEAR_QUESTIONS'}
+
+const questionReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'CREATE_QUESTION':
+      return Object.assign(state, {
+        [action.questionId]:{
+          questionText: action.questionText,
+          votes: 1
+        }
+      });
+    case 'UPVOTE': 
+      return Object.assign(state, {
+        [action.questionId]: Object.assign(state[action.questionId], state[action.questionId].votes++)
+      });
+    case 'CLEAR_QUESTIONS':
+      return {};
+    default:
+      return state;
+  }
+};
+export default questionReducer;
---
+++ test/questionReducerTest.js
@@ -0,0 +1,46 @@
+// tests for question reducer
+const assert = require('assert');
+const deepFreeze = require('deep-freeze');
+import  questionReducer  from '../client/src/reducers/questionReducer.jsx';
+
+//POINTERS:
+// 1) place an x before a block of tests or an individual test to disable that test as pending
+// 2) use deepFreeze to test purity of inputs
+
+describe('questionReducer', function() {
+  var testState = {
+    324234:{votes: 3, questionText: 'sample question'},
+    435353:{votes: 1, questionText: 'Who am I?'}
+  }
+  
+  deepFreeze(testState);
+  describe('UNDEFINED ACTION', function(){
+    it('should return default state if action is undefined', function () {
+      assert.deepEqual(testState, questionReducer(testState, {type: 'TEST'}));
+    });
+  });
+
+  describe('CREATE_QUESTION', function () {
+    it('it should add a new question to the store', function () {
+      assert.deepEqual(testState,
+        questionReducer({324234:{votes: 3, questionText: 'sample question'}},
+          {type: 'CREATE_QUESTION', questionId: 435353, questionText: 'Who am I?'}));
+    });
+  });
+
+  describe('UPVOTE', function () {
+    it('it should increment the question matching the questionId', function() {
+      assert.deepEqual({324234:{votes: 3, questionText: 'sample question'}},
+        questionReducer({324234:{votes: 2, questionText: 'sample question'}},
+        {type: 'UPVOTE', questionId: 324234}));
+    });
+  });
+
+  describe('CLEAR_QUESTIONS', function () {
+    it('it should clear the store of all questions', function() {
+      assert.deepEqual({},
+        questionReducer(testState,
+        {type: 'CLEAR_QUESTIONS'}));
+    });
+  });
+});
