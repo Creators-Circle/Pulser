5c5cc28f9a67d91615ebc9428746e9c4d49ece2b
Ari Frankel <ari.l.frankel@gmail.com>
2017-01-09 17:09:07 -0600

    thumbsreducer: bugfix (typo) and add tests for down/side thumbs

--- test/thumbsReducerTest.js
+++ test/thumbsReducerTest.js
@@ -31,11 +31,21 @@ describe('thumbsReducer', function() {
     });
   });
 
-  xdescribe('THUMB_CLICKED', function () {
-    it('it should increment the specific thumbType included in the action', function() {
+  describe('THUMB_CLICKED', function () {
+    it('it should increment the specific thumbType included in the action - up', function() {
       assert.deepEqual({topicId: '', displayed: false, up: 1, down: 0, side: 0},
         thumbsReducer(testState,
-        {type: 'THUMB_CLICKED', thumbType: 'up'}));
+        {type: 'THUMB_CLICKED', thumbChoice: 'up'}));
+    });
+    it('it should increment the specific thumbType included in the action - down', function() {
+      assert.deepEqual({topicId: '', displayed: false, up: 0, down: 1, side: 0},
+        thumbsReducer(testState,
+        {type: 'THUMB_CLICKED', thumbChoice: 'down'}));
+    });
+    it('it should increment the specific thumbType included in the action - side', function() {
+      assert.deepEqual({topicId: '', displayed: false, up: 0, down: 0, side: 1},
+        thumbsReducer(testState,
+        {type: 'THUMB_CLICKED', thumbChoice: 'side'}));
     });
   });
 
