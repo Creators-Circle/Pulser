b31e9255e6b38814ccc24de8d298f13cec33a4f0
Ari Frankel <ari.l.frankel@gmail.com>
2017-01-12 11:40:37 -0600

    layout authpage

--- client/src/components/PresenterView.jsx
+++ client/src/components/PresenterView.jsx
@@ -66,7 +66,11 @@ class PresenterView extends Component {
         this.setState({audience: --this.state.audience});
       }
     });
-  }
+
+    socket.on('stopPresentation', function () {
+      socket.disconnect();
+    });
+  };
 
   // showStore () {
   //   console.log(store.getState());
