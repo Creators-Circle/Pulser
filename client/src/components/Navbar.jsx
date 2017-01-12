4fb8814c530e892643bd20186dacb2e1e7f8ba48
XianAquino <xian_a@yahoo.com>
2017-01-12 11:53:45 -0600

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import LogoutButton from './LogoutButton';
 import UserInfo from './UserInfo';
 import '../css/Navbar.css';
+import { Link } from 'react-router';
 
 class Navbar extends Component {
   render () {
     return (
       <div id="NavbarContainer">
-        <div id="BrandContainer"><img id="logo" className="navbarElement" src='./img/logo.png'/>FollowMe</div>
+        <div id="BrandContainer"><img id="logo" className="navbarElement" src='/../img/logo.png'/>
+          <Link to={'/'}><span>FollowMe</span></Link>
+        </div>
         <LogoutButton/>
         <UserInfo/>
       </div>
