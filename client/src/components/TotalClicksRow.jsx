8348f5cad5fe13322d7ac2b72873fb39251473ee
XianAquino <xian_a@yahoo.com>
2017-01-12 11:09:53 -0600

    Fix user table design

--- client/src/components/TotalClicksRow.jsx
+++ client/src/components/TotalClicksRow.jsx
@@ -5,7 +5,7 @@ const TotalClicksRow = (props) => {
   return (
     <tr>
       <td onClick = { () => { props.displayUserSummary(props.userId); } }>
-        <img id='profilePic' src={props.avatar} /><span>{props.name}</span>
+        <img className='audience-pic' src={props.avatar} /><span>{props.name}</span>
         </td>
       <td className='text-center'>{props.noOfClicks}</td>
     </tr>
