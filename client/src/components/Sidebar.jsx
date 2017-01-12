91ec364b0ada1c9e0e4ac815154927a7a519422d
Ari Frankel <ari.l.frankel@gmail.com>
2017-01-12 09:55:58 -0600

    defualt timer back to display

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -93,7 +93,7 @@ class Sidebar extends Component {
       <div id="Sidebar">
         <ul id="SidebarElements">
           <li><TitleBar className='title-bar'/></li>
-          <div className='timer' style={{display: 'none'}}>
+          <div className='timer'>
             <div className='clock'>Time: {this.props.time}</div>
             <div>Duration: {this.props.duration}</div>
           </div>
